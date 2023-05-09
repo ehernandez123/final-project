797d401c20c2549f9e16cbf5a66ab653016d9980
Erica Carmona <erica@Erica-2.local>
2023-05-09 00:01:24 -0700

    navbar anination working needs styling on buttons.. added as place holders

---
+++ navbar/navbar.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+        
+        // Scroll event listener
+        const logo1 = document.querySelector('.logo1');
+        const logo2 = document.querySelector('.logo2');
+
+        window.addEventListener('scroll', function() {
+            let scrollPercentage = Math.min(window.scrollY / 50, 1);
+            logo1.style.opacity = 1 - scrollPercentage;
+            logo2.style.clipPath = `polygon(0 0, ${scrollPercentage * 100}% 0, ${scrollPercentage * 100}% 100%, 0 100%)`;
+        });
+    })
+    .catch(error => {
+        console.error("Error loading navbar:", error);
+    });
+});
